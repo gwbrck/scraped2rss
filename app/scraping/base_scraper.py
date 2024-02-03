d8075a33889dd705a3bea65df05c2379a53b467b
gregor <g@wbrck.de>
2024-02-03 23:13:41 +0100

    fix cdata

--- app/scraping/base_scraper.py
+++ app/scraping/base_scraper.py
@@ -58,7 +58,7 @@ class BaseScraper:
                 e.description(item['description'])
 
             if 'content' in item:
-                e.content(item['content'])
+                e.content(item['content'], type="CDATA")
 
             if 'title' in item:
                 e.title(item['title'])
